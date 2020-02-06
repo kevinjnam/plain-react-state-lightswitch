bc3ae084250251e775393066f529215f64be3a9f
Kevin Nam <kevinjnam@gmail.com>
2020-02-05 18:55:41 -0800

    changed to redux and used react-redux hooks

--- src/index.js
+++ src/index.js
@@ -1,28 +1,46 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { useSelector, useDispatch, Provider } from 'react-redux'
+import { createStore } from 'redux';
 import "./index.css";
 
-class Room extends React.Component {
-  state = {
-    isLightOn: true
-  };
+const initialState = {
+  isLightOn: true
+}
 
-  flipLight = () => {
-    this.setState({
-      isLightOn: !this.state.isLightOn
-    });
-  };
-
-  render() {
-    const lightedness = this.state.isLightOn ? "lit" : "dark";
-    return (
-      <div className={`room ${lightedness}`}>
-        the room is {lightedness}
-        <br />
-        <button onClick={this.flipLight}>flip</button>
-      </div>
-    );
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case 'FLIP_LIGHT': 
+      return {
+        ...state,
+        isLightOn: !state.isLightOn
+     }
+    default:
+      return state
   }
 }
 
-ReactDOM.render(<Room />, document.getElementById("root"));
+const store = createStore(
+  reducer
+)
+
+const Room = () => {
+  const { isLightOn } = useSelector(state => state)
+  const dispatch = useDispatch();
+  const flipLight = () => dispatch({type: 'FLIP_LIGHT'})
+
+  const lightedness = isLightOn ? "lit" : "dark";
+  return (
+    <div className={`room ${lightedness}`}>
+     the room is {lightedness}
+     <br />
+     <button onClick={flipLight}>flip</button>
+  </div>
+  )
+}
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Room />
+  </Provider>
+, document.getElementById("root"));
